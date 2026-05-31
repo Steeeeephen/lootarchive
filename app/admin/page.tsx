7026b61f03a1a8bbda7a442de02b8c64db16340c
Stephen Zalalas <stephenzalalas@proton.me>
2026-05-31 14:11:26 -0400

    Update 404 page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 
 const Page = () => {
-
-
     return (
         <div>Admin Dashboard</div>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,12 @@
 
 export default function Home() {
   return (
-    <div>
+    <>
       <main>
           <div>
 
-              <h1 className="text-6xl font-grenze-gotisch text-foreground">The Loot Archive</h1>
-              <p className="font-pixel">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque dolorem fuga itaque minima nam recusandae sed, veritatis vero voluptatum. Beatae magni, maiores non nostrum omnis porro quis sunt voluptatum! Quidem!</p>
-              <p className="font-garamond">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, commodi debitis ea eaque eius eveniet explicabo fuga illo laborum nemo nisi nobis odio quia quos rerum similique soluta voluptatem! Voluptate!</p>
         </div>
       </main>
-    </div>
+    </>
   );
 }
