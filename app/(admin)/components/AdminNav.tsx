50485f6af6f0435e27db0505eb88c581ea375019
Stephen Zalalas <stephenzalalas@proton.me>
2026-06-07 13:59:25 -0400

    Formatting

--- app/(admin)/components/AdminNav.tsx
+++ app/(admin)/components/AdminNav.tsx
@@ -4,12 +4,12 @@ import Link from "next/link";
 
 const AdminNav = () => {
     return (
-        <Sidebar className="bg-sidebar">
+        <Sidebar className="bg-sidebar ">
             <p className="text-xl mb-6 ml-6">
                 <span className="font-grenze-gotisch text-2xl font-bold">Loot Archive</span> Admin
             </p>
 
-            <SidebarContent className="flex flex-col gap-2 ml-6">
+            <SidebarContent className="flex flex-col gap-2 ml-6 ">
                 <Link className="admin-link" href="/admin/orders">Orders</Link>
                 <Link className="admin-link" href="/admin/customers">Customers</Link>
                 <Link className="admin-link" href="/admin/categories">Categories</Link>
