63decb33af11a5cc5204011fa9c35a0dbf6f4653
Stephen Zalalas <stephenzalalas@proton.me>
2026-06-10 17:35:46 -0400

    Extract the brand table to its own component

---
+++ app/(admin)/admin/brands/BrandsTable.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from "@/components/ui/table";
+import {getBrands} from "@/lib/data/brands";
+import {Input} from "@/components/ui/input";
+import Link from "next/link";
+import {Button} from "@/components/ui/button";
+
+const BrandsTable = async () => {
+
+    const allBrands = await getBrands();
+
+
+    return (
+        <Table>
+            <TableHeader>
+                <TableRow className="table-row text-center">
+                    <TableHead className="table-head"></TableHead>
+                    <TableHead className="table-head">ID</TableHead>
+                    <TableHead className="table-head">Name</TableHead>
+                    <TableHead className="table-head">Slug</TableHead>
+                    <TableHead className="table-head">Actions</TableHead>
+                </TableRow>
+            </TableHeader>
+
+            <TableBody>
+                { allBrands.map((brand) => (
+                    <TableRow className="" key={brand.id}>
+                        <TableCell className="table-cell"><Input className="" type="checkbox" value={brand.id} aria-label="Select Brand" /></TableCell>
+                        <TableCell className="table-cell">{brand.id}</TableCell>
+                        <TableCell className="table-cell">{brand.name}</TableCell>
+                        <TableCell className="table-cell">{brand.slug}</TableCell>
+                        <TableCell className="table-cell">
+                            <Link href={`/admin/brands/${brand.slug}`}><Button variant="outline" className="cursor-pointer">View</Button></Link>
+                        </TableCell>
+                    </TableRow>
+                )) }
+            </TableBody>
+
+        </Table>    )
+}
+export default BrandsTable
--- app/(admin)/admin/brands/page.tsx
+++ app/(admin)/admin/brands/page.tsx
@@ -1,15 +1,11 @@
 import React from 'react'
 import {Button} from "@/components/ui/button";
 import Link from "next/link";
-
-import {getBrands} from "@/lib/data/brands";
-import {Table, TableHeader, TableRow, TableHead, TableBody, TableCell} from "@/components/ui/table";
-import {Input} from "@/components/ui/input";
 import PageBreadcrumb from "@/components/PageBreadcrumb";
+import BrandsTable from "@/app/(admin)/admin/brands/BrandsTable";
 
 const Page = async () => {
 
-    const allBrands = await getBrands();
 
 
     return (
@@ -17,42 +13,9 @@ const Page = async () => {
             <PageBreadcrumb />
             <div className="admin-header">
                 <h1 className="admin-header__title">Brand Management</h1>
-
                 <Link href="/admin/brands/create" ><Button variant="outline" className="cursor-pointer">Create Brand</Button></Link>
-
             </div>
-
-
-
-            <Table>
-                <TableHeader>
-                    <TableRow className="table-row text-center">
-                        <TableHead className="table-head"></TableHead>
-                        <TableHead className="table-head">ID</TableHead>
-                        <TableHead className="table-head">Name</TableHead>
-                        <TableHead className="table-head">Slug</TableHead>
-                        <TableHead className="table-head">Actions</TableHead>
-                    </TableRow>
-                </TableHeader>
-
-                <TableBody>
-                    { allBrands.map((brand) => (
-                        <TableRow className="" key={brand.id}>
-                            <TableCell className="table-cell"><Input className="" type="checkbox" value={brand.id} aria-label="Select Brand" /></TableCell>
-                            <TableCell className="table-cell">{brand.id}</TableCell>
-                            <TableCell className="table-cell">{brand.name}</TableCell>
-                            <TableCell className="table-cell">{brand.slug}</TableCell>
-                            <TableCell className="table-cell">
-                                <Link href={`/admin/brands/${brand.slug}/edit`}><Button variant="outline" className="cursor-pointer">View</Button></Link>
-                            </TableCell>
-                        </TableRow>
-                    )) }
-                </TableBody>
-
-            </Table>
-
-
-
+            <BrandsTable />
         </main>
     )
 }
