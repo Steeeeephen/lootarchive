74cb29a222f30a4dd63b3e4f29b451872f27e781
Stephen Zalalas <stephenzalalas@proton.me>
2026-06-23 17:46:40 -0400

    Create category slug page

--- app/(admin)/admin/categories/[slug]/page.tsx
+++ app/(admin)/admin/categories/[slug]/page.tsx
@@ -1,13 +1,38 @@
 import PageBreadcrumb from "@/components/PageBreadcrumb";
+import {getCategoryBySlug} from "@/lib/data/categories";
+import CategoryForm from "@/app/(admin)/admin/categories/CategoryForm";
+import Image from 'next/image'
 
-const Page = () => {
+
+interface pageProps {
+    params: Promise<{ slug:string }>
+}
+
+const Page = async ({params} :pageProps) => {
+
+    const { slug } = await params;
+
+    const category = await getCategoryBySlug(slug)
+
     return (
         <>
             <main>
                 <PageBreadcrumb />
 
+                <div className="admin-header">
+                    <h1 className="admin-header__title">
+                        Editing {category.name}
+                    </h1>
+                </div>
+
+                {
+                    category?.image && (
+                        <Image src={category.image} alt="Current image" width={128} height={128} />
+                    )
+                }
 
 
+                <CategoryForm category={category} />
 
             </main>
 
