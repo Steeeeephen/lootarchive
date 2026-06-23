8792206c52922eee5589671089896142b7c6a7b5
Stephen Zalalas <stephenzalalas@proton.me>
2026-06-23 17:34:18 -0400

    Move category data table to correct component.

--- app/(admin)/admin/categories/page.tsx
+++ app/(admin)/admin/categories/page.tsx
@@ -2,52 +2,28 @@ import React from 'react'
 import {Button} from "@/components/ui/button";
 import Link from "next/link";
 
-import {getBrands} from "@/lib/data/brands";
-import {Table, TableHeader, TableRow, TableHead, TableBody, TableCell} from "@/components/ui/table";
-import {Input} from "@/components/ui/input";
+import PageBreadcrumb from "@/components/PageBreadcrumb";
+import CategoriesTable from "@/app/(admin)/admin/categories/CategoriesTable";
 
 const Page = async () => {
 
-    // const allBrands = await getBrands();
 
 
     return (
         <main>
+            <PageBreadcrumb />
             <div className="admin-header">
                 <h1 className="admin-header__title">Category Management</h1>
 
-                <Link href="/admin/category/create" ><Button variant="outline" className="cursor-pointer">Create Category</Button></Link>
+                <Link href="/admin/categories/create" ><Button variant="outline" className="cursor-pointer">Create Category</Button></Link>
 
             </div>
 
 
+        <CategoriesTable />
 
-            <Table>
-                <TableHeader>
-                    <TableRow className="table-row text-center">
-                        <TableHead className="table-head"></TableHead>
-                        <TableHead className="table-head">ID</TableHead>
-                        <TableHead className="table-head">Name</TableHead>
-                        <TableHead className="table-head">Slug</TableHead>
-                        <TableHead className="table-head">Actions</TableHead>
-                    </TableRow>
-                </TableHeader>
 
-                {/*<TableBody>*/}
-                {/*    { allBrands.map((brand) => (*/}
-                {/*        <TableRow className="" key={brand.id}>*/}
-                {/*            <TableCell className="table-cell"><Input className="" type="checkbox" value={brand.id} aria-label="Select Brand" /></TableCell>*/}
-                {/*            <TableCell className="table-cell">{brand.id}</TableCell>*/}
-                {/*            <TableCell className="table-cell">{brand.name}</TableCell>*/}
-                {/*            <TableCell className="table-cell">{brand.slug}</TableCell>*/}
-                {/*            <TableCell className="table-cell">*/}
-                {/*                <Link href={`/admin/brands/${brand.id}/edit`}><Button variant="outline" className="cursor-pointer">View</Button></Link>*/}
-                {/*            </TableCell>*/}
-                {/*        </TableRow>*/}
-                {/*    )) }*/}
-                {/*</TableBody>*/}
 
-            </Table>
 
 
 
